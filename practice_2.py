3a4e332b926468e7f87253daa7a9072617f12176
Laura <lhang@onshift.com>
2018-02-01 12:22:43 -0500

    Add more Python datatype katas

---
+++ practice_2.py
@@ -0,0 +1,16 @@
+# DATA TYPES
+
+# str
+_str = 'test'
+_str2 = "tst"
+_str3 = f'{_str2} test'.format(_str2=_str2)
+
+# bool
+_bool = True
+_bool_false = False
+
+
+# int
+_int = 1
+_int = -1
+_int_negative = -1
